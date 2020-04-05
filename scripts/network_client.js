d513bf5d63eeb1894c4d335407671e42ce6be056
Menno van der Graaf <mennovandergraaf@hotmail.com>
2020-04-06 01:08:14 +0200

    Basically add entire game

---
+++ scripts/network_client.js
@@ -0,0 +1,25 @@
+// You're an absolute dork btw
+
+const ID_LENGTH = 5;
+
+class NetworkClient {
+  constructor() {
+    do {
+      this.id_ = this.generateId()
+    } while (this.checkIdExist(this.id_));
+
+    console.log(this.id_);
+  }
+
+  generateId() {
+    var random_string = btoa(Math.random());
+    var id_string = random_string.slice(random_string.length - ID_LENGTH);
+    return id_string;
+  }
+
+  checkIdExist() {
+    return false;
+  }
+}
+
+var test = new NetworkClient;
