a44177100f68e6eae104899b27b842f9f7cb2304
Menno van der Graaf <mennovandergraaf@hotmail.com>
2020-05-01 22:11:04 +0200

    Add basic server connection

--- scripts/README.md
+++ scripts/README.md
@@ -1,2 +1,14 @@
-# Scripts for Tic Tac Test
-This directory holds the scripts
+# Scripts Directory for Tic Tac Test
+
+There are some files here, they do things
+
+## network_client.js
+
+This is my thing, I make (-Menno). It implements a JavaScript class called
+NetworkClient. Which can be used to send and receive game data over a MQTT server.
+The current interface is:
+
+### `constructor()`
+This creates an instance of the class. It automatically connects to the MQTT server,
+creates a new game ID, and subscribes to the MQTT topics for that game.
+
--- scripts/network_client.js
+++ scripts/network_client.js
@@ -3,15 +3,37 @@
 const ID_LENGTH = 5;
 
 class NetworkClient {
+  url = "mqtt.eclipse.org";
+  port = 443;
+  root = "com.mercotui.tic_tac_test";
+
   constructor() {
+    // Create a client instance
+    this.mqtt_client = new Paho.MQTT.Client(this.url, this.port, this.generateClientId());
+
+    // set callback handlers
+    this.mqtt_client.onConnectionLost = this.onConnectionLost;
+    this.mqtt_client.onMessageArrived = this.onMessageArrived;
+
+    // connect the client
+    this.mqtt_client.connect({onSuccess:this.onConnect.bind(this)});
+
+    // create a new game
     do {
-      this.id_ = this.generateId()
+      this.id_ = this.generateGameId()
     } while (this.checkIdExist(this.id_));
 
     console.log(this.id_);
   }
 
-  generateId() {
+  generateClientId() {
+    // uuid v4 from https://stackoverflow.com/a/2117523/4729813
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+
+  generateGameId() {
     var random_string = btoa(Math.random());
     var id_string = random_string.slice(random_string.length - ID_LENGTH);
     return id_string;
@@ -20,6 +42,23 @@ class NetworkClient {
   checkIdExist() {
     return false;
   }
-}
 
-var test = new NetworkClient;
+  onConnect = () => {
+    console.log("onConnect");
+    this.mqtt_client.subscribe(this.root + "/test");
+    var message = new Paho.MQTT.Message("Hello from " + this.mqtt_client.clientId);
+    message.destinationName = this.root + "/test";
+    this.mqtt_client.send(message);
+  }
+
+  onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+  }
+
+}
